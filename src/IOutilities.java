5f98b4336ccb18d53e557d5adf7b27aff71eb83a
Lukas Szerszen <lukassz@kth.se>
2019-02-17 14:46:57 +0100

    Fix #8, remove uneccesary lambda

--- src/IOutilities.java
+++ src/IOutilities.java
@@ -23,7 +23,6 @@ public class IOutilities {
     }
 
     public static Stream<String> streamSupplier(final String[] input){
-        Supplier<Stream<String>> supplier = () -> Stream.of(input);
-        return supplier.get();
+        return Stream.of(input);
     }
 }
